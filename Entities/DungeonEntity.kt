9aacc2ce9108b752f7ec86e230758924b64fa15d
nikolaimerritt <nmerritt.62761@farnborough.ac.uk>
2017-09-27 12:12:06 +0100

    Update DungeonEntity.kt

--- Entities/DungeonEntity.kt
+++ Entities/DungeonEntity.kt
@@ -1,9 +1,11 @@
-abstract class DungeonEntity
+import java.awt.Image
+
+abstract class DungeonEntity (val sprite: Image, val name: String)
 {
     var x = 0; private set
     var y = 0; private set
     
-    protected fun move(newX: int, newY: int)
+    protected fun move(newX: Int, newY: Int)
     {
         // do some error checking stuff here
         x = newX
