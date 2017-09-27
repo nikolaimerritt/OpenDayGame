3d5d805e1dd27cf5c0e5ffb1e4dcbc4c4d092205
nikolaimerritt <nmerritt.62761@farnborough.ac.uk>
2017-09-27 11:28:01 +0100

    Added a draft for move()

--- Entities/DungeonEntity.kt
+++ Entities/DungeonEntity.kt
@@ -1,5 +1,12 @@
 abstract class DungeonEntity
 {
-    var x = 0; protected set
-    var y = 0; protected set
+    var x = 0; private set
+    var y = 0; private set
+    
+    protected fun move(newX: int, newY: int)
+    {
+        // do some error checking stuff here
+        x = newX
+        y = newY
+    }
 }
